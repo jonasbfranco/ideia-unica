1c0a6dc25199fc1431a4b8c260b45a78af9f4101
jonasbfranco <jonasbfranco@hotmail.com>
2021-05-22 16:38:40 -0300

    corrigindo home

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,11 @@ import { useState } from 'react';
 function Home() {
     return  (
         <div>
-            <h1>Home Jonas</h1>
-            <Contador />
+            <h1>Home</h1>
         </div>
     )
 }
 
-function Contador() {
-    const [contador, SetContador] = useState(1);
 
-    function adicionarContador() {
-        SetContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 
 export default Home
