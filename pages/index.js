45dd3f6fcca80aea77af01bcb478af9a573657a0
jonasbfranco <jonasbfranco@hotmail.com>
2021-05-22 16:40:56 -0300

    teste de pagina home

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,19 @@ function Home() {
     )
 }
 
+function Contador() {
+    const [contador, SetContador] = useState(1);
 
+    function adicionarContador() {
+        SetContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
 
 export default Home
