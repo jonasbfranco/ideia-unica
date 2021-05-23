2060d5e9773b166a967ebf6ff5d9a4d2c7016c0a
jonasbfranco <jonasbfranco@hotmail.com>
2021-05-23 16:50:18 -0300

    page tempo

--- pages/tempo_delay.js
+++ pages/tempo_delay.js
@@ -7,7 +7,7 @@ function Tempo(props) {
     return  (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático)</div>
+            <div>{props.staticDateString} (estático - com delay)</div>
         </div>
     )
 }
@@ -15,17 +15,21 @@ function Tempo(props) {
 
 export async function getStaticProps() {
     console.log('> Passando pelo getStaticProps();');
+    console.log('> Adicionando delay de 5 segundos');
 
+    await delay(5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
     return {
         props: {
             staticDateString
-        },
-        revalidate: 1
+        }
     }
 }
+
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 
 export default Tempo;
