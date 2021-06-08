490ba355bc498c134bb434f6e8e7d7057b711e6e
jonasbfranco <jonasbfranco@hotmail.com>
2021-06-07 23:20:34 -0300

    TEMPO JS

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
+    const subscribeResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    const subscribeResponseJson = await subscribeResponse.json();
+    const inscritos = subscribeResponseJson.total_subscribers;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
     });
 }
 
